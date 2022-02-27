deb9d74f54210ebd36aec76310bb79904f874044
= <=>
2022-02-28 00:13:50 +0100

    uncommented run() command in run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from utils import tokenize
 from classification_messages_app import app
 
-#def main():
-#    app.run(host='0.0.0.0', port=3001, debug=True)
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
